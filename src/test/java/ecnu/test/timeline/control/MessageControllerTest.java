8e8f81bb7920770426fb237c5658d0323ad3c557
whitfield <10842089@qq.com>
2019-11-11 18:48:15 +0800

    Fix displayname

--- src/test/java/ecnu/test/timeline/control/MessageControllerTest.java
+++ src/test/java/ecnu/test/timeline/control/MessageControllerTest.java
@@ -2,22 +2,26 @@ package ecnu.test.timeline.control;
 
 import ecnu.test.timeline.service.MessageService;
 import ecnu.test.timeline.service.MessageServiceImpl;
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(SpringExtension.class)
 @RunWith(SpringRunner.class)
 @WebMvcTest(MessageController.class)
 public class MessageControllerTest {
@@ -35,7 +39,7 @@ public class MessageControllerTest {
     MessageMapper messageMapper;
 
     @Test
-    @DisplayName("测试getMessageList()方法")
+    @DisplayName("Testing getMessageList()")
     public void shouldGetMessageList() throws Exception{
         ResultActions perform = mockMvc.perform(get("/api/message"));
         perform.andExpect(status().isOk());
@@ -44,7 +48,7 @@ public class MessageControllerTest {
     }
 
     @Test
-    @DisplayName("测试getMessageByID()方法")
+    @DisplayName("Testing getMessageByID(int id)")
     public void shouldGetMessageByID() throws Exception{
         ResultActions perform = mockMvc.perform(get("/api/message/1"));
         perform.andExpect(status().isOk());
@@ -53,7 +57,7 @@ public class MessageControllerTest {
     }
 
     @Test
-    @DisplayName("测试addTestMessage()方法")
+    @DisplayName("Testing addTestMessage()")
     public void shouldAddTestMessage() throws Exception{
         ResultActions perform = mockMvc.perform(post("/api/message"));
         perform.andExpect(status().isCreated());
@@ -62,7 +66,7 @@ public class MessageControllerTest {
     }
 
     @Test
-    @DisplayName("测试deleteMessageByID()方法")
+    @DisplayName("Testing deleteMessageByID(int id)")
     public void shouldDeleteMessageByID() throws Exception{
         ResultActions perform = mockMvc.perform(delete("/api/message/11"));
         perform.andExpect(status().isNoContent());
